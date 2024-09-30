204a198b8c4fc9ab4b6b9730e94cfd3f61e6d3dc
KillSwitch1386 <sebastien.morio@hotmail.com>
2024-09-30 11:30:22 -0600

    added shooting features

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,10 +17,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()    
@@ -32,6 +35,11 @@ def main():
         screen.fill("black")
         for object in updatable:
             object.update(dt)
+        for asteroid in asteroids:
+            if (asteroid.check_for_collision(player)):
+                print("Game over!")
+                exit()
+
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
